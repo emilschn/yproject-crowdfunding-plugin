e76837df3c4ad16a32102d8cce6c33252900d70d
Boubacr KOITA <boubacar.koita@gmail.com>
2013-09-06 13:46:23 +0200

    AMélioration du graphique du formulaire de vote

--- includes/shortcode-vote-form.php
+++ includes/shortcode-vote-form.php
@@ -127,60 +127,65 @@ function ypcf_shortcode_printPageVoteForm($post, $campaign) {
 ?>
             <!--Formulaire de soumission de vote, visible depuis la page project des projets en vote-->
     <div class="left post_bottom_infos">
-        <form name="ypvote" action="<?php get_permalink();?>" method="POST" class="ypvote-form" enctype="multipart/form-data">
-            
-                <fieldset>
-                    <legend>Votez sur ce projet</legend>
-                   
-                    <input id="impact-positif" type="radio" name="impact"  value="positif" checked="checked">Je pense que ce projet va avoir un impact positif</input></br>
+        <div class="post_bottom_buttons">
+            <div class="dark" style="color:white;text-transform:none;padding-left:5px;">
+                <legend>Votez sur ce projet</legend>
+            </div>
+            <div class="light" style="text-transform:none;text-align : left; padding-left:5px;" >
+            <form name="ypvote" action="<?php get_permalink();?>" method="POST" class="ypvote-form" enctype="multipart/form-data">
+                       
+                        <input id="impact-positif" type="radio" name="impact"  value="positif" checked="checked">Je pense que ce projet va avoir un impact positif</input></br>
 
-                    <span id="impact-positif-content" style="display: ">
-                        <input type="checkbox" name="local"  value="local">Local</input></br>
-                        <input type="checkbox" name="environmental" value="environmental">Environnemental</input></br>
-                        <input type="checkbox" name="social" value="social">Social</label></br>
-                        <input type="checkbox" name="autre" value="autre">Autre</input>
-                        <input id="precision" name="precision" type="text" placeholder="tapez ici" />
-                    </span></br>
-                    
-                    <input id="desaprouve" type="radio" name="impact" value="negatif" >Je d&egravesapprouve ce projet car son impact pr&eacutevu n'est pas significatif</input></br></br>
+                        <span id="impact-positif-content" style="display: ">
+                            <input type="checkbox" name="local"  value="local">Local</input></br>
+                            <input type="checkbox" name="environmental" value="environmental">Environnemental</input></br>
+                            <input type="checkbox" name="social" value="social">Social</label></br>
+                            <input type="checkbox" name="autre" value="autre">Autre</input>
+                            <input id="precision" name="precision" type="text" placeholder="tapez ici" />
+                        </span></br>
+                        
+                        <input id="desaprouve" type="radio" name="impact" value="negatif" >Je d&egravesapprouve ce projet car son impact pr&eacutevu n'est pas significatif</input></br></br>
 
-                    
-                    <input id="pret" type="radio" name="maturite" value="pret" checked="checked">Je pense que ce projet est pr&ecirct pour la collecte</input></br>
-                    <span id="content-pret">
-                        <label  id="investir" name="investir" value="investir">Je serais pr&ecirct &agrave investir</label></br>
-                        <input id="sum" name="sum" type="text" placeholder="200 euro" v/></br>
-                     
-                        <label class="risque" name="risque" value="risque">Risque li&eacute &aring ce projet</label></br>
-                        <select id="liste_risque" name="liste_risque"  placeholder="choisir le type de risque">
-                            <option ></option>
-                            <option id="risque_tres_faible">Le risque tr&egraves faible</option>
-                            <option id="risque_plutot_faible">Le risque plut&ocirct faible</option>
-                            <option id="risque_modere">Le risque mod&eacuter&eacute</option>
-                            <option id="risque_plutot_eleve">Le risque plut&ocirct &eacutelev&eacute</option>
-                            <option id="risque_tres_eleve">Le risque tr&egraves &eacutelev&eacute</option>
-                        </select>
-                    </span></br></br>
+                        
+                        <input id="pret" type="radio" name="maturite" value="pret" checked="checked">Je pense que ce projet est pr&ecirct pour la collecte</input></br>
+                        <span id="content-pret">
+                            <label  id="investir" name="investir" value="investir">Je serais pr&ecirct &agrave investir</label></br>
+                            <input id="sum" name="sum" type="text" placeholder="200 euro" v/></br>
+                         
+                            <label class="risque" name="risque" value="risque">Risque li&eacute &aring ce projet</label></br>
+                            <select id="liste_risque" name="liste_risque"  placeholder="choisir le type de risque">
+                                <option ></option>
+                                <option id="risque_tres_faible">Le risque tr&egraves faible</option>
+                                <option id="risque_plutot_faible">Le risque plut&ocirct faible</option>
+                                <option id="risque_modere">Le risque mod&eacuter&eacute</option>
+                                <option id="risque_plutot_eleve">Le risque plut&ocirct &eacutelev&eacute</option>
+                                <option id="risque_tres_eleve">Le risque tr&egraves &eacutelev&eacute</option>
+                            </select>
+                        </span></br></br>
 
-                    
-                    <input id="retravaille" type="radio" name="maturite" value="retravaille">Je pense que ce projet doit &ecirctre retravaill&eacute avant de pouvoir &ecirctre financ&eacute. Sur quels points </input></br></br>
-                    <span id="content-retravaille">
-                        <input type="checkbox" name="pas_responsable" value="pas_responsable">Pas d&acuteimpact responsable</input></br>
-                        <input type="checkbox" name="mal_explique" value="mal_explique"> Projet mal expliqu&eacute  </input></br>
-                        <input type="checkbox" name="qualite_produit" value="qualite_produit">Qualit&eacute du produit/service</input></br>
-                        <input type="checkbox" name="qualite_equipe" value="qualite_equipe">Qualit&eacute de l&acute&eacutequipe</input></br>
-                        <input type="checkbox" name="qualite_business_plan" value="qualite_business_plan">Qualit&eacute du business plan</input></br>
-                        <input type="checkbox" name="qualite_innovation" value="qualite_innovation">Qualit&eacute d&acuteinnovation</input></br>
-                        <input type="checkbox" name="qualite_marche" value="qualite_marche" >Qualit&eacute du march&eacute, porteur</input></br>
-            
-                        <label> Autre</label>
-                        <input type="text" name="other" id="other" ></input></br></br>
-                    </span>
-                    <label> Conseil</label><br>
-                    <textarea type="text" name="conseil" id="conseil" value="conseil"></textarea></br>
-                    <br><input type="submit" name="submit" value= "valider" />
-                 </fieldset>
+                        
+                        <input id="retravaille" type="radio" name="maturite" value="retravaille">Je pense que ce projet doit &ecirctre retravaill&eacute avant de pouvoir &ecirctre financ&eacute. Sur quels points </input></br></br>
+                        <span id="content-retravaille">
+                            <input type="checkbox" name="pas_responsable" value="pas_responsable">Pas d&acuteimpact responsable</input></br>
+                            <input type="checkbox" name="mal_explique" value="mal_explique">Projet mal expliqu&eacute  </input></br>
+                            <input type="checkbox" name="qualite_produit" value="qualite_produit">Qualit&eacute du produit/service</input></br>
+                            <input type="checkbox" name="qualite_equipe" value="qualite_equipe">Qualit&eacute de l&acute&eacutequipe</input></br>
+                            <input type="checkbox" name="qualite_business_plan" value="qualite_business_plan">Qualit&eacute du business plan</input></br>
+                            <input type="checkbox" name="qualite_innovation" value="qualite_innovation">Qualit&eacute d&acuteinnovation</input></br>
+                            <input type="checkbox" name="qualite_marche" value="qualite_marche" >Qualit&eacute du march&eacute, porteur</input></br>
+                
+                            <label> Autre</label>
+                            <input type="text" name="other" id="other" ></input></br></br>
+                        </span>
+                        <label> Conseil</label><br>
+                        <textarea type="text" name="conseil" id="conseil" value="conseil"></textarea></br>
+                        <br><input type="submit" name="submit" value= "valider" />
+                    </fieldset>
 
-         </form> 
+             </form>
+         </div>
+        </div> 
+         
     </div>
         
 <?php
