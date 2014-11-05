2c705b0089375b645851b501587dd8db5e7fd3ef
Matthieu Moreau <matthieu.moreau@hetic.net>
2014-11-05 21:49:28 +0100

    Mise à jour de BoppLib pour l'API Bopp v2

--- includes/bopp/bopp-lib.php
+++ includes/bopp/bopp-lib.php
@@ -180,9 +180,9 @@ class BoppLib {
 	public static function create_user($wp_user_id, $first_name, $last_name) {
 		$request_params = array(
 			'users' => array(
-				'wpUserId' => $wp_user_id,
-				'userName' => $first_name,
-				'userSurname' => $last_name
+				'user_wp_id' => $wp_user_id,
+				'user_name' => $first_name,
+				'user_surname' => $last_name
 			)
 		);
 		$result_obj = BoppLib::call_post('users', $request_params);
@@ -207,9 +207,9 @@ class BoppLib {
 	 */
 	public static function update_user($id, $first_name, $last_name) {
 		$request_params = array(
-			'users' => array(
-				'userName' => $first_name,
-				'userSurname' => $last_name
+			'user' => array(
+				'user_name' => $first_name,
+				'user_surname' => $last_name
 			)
 		);
 		$result_obj = BoppLib::call_put('users/' . $id, $request_params);
@@ -242,8 +242,8 @@ class BoppLib {
 	public static function create_project($wp_project_id, $wp_project_name) {
 		$request_params = array(
 			'projects' => array(
-				'wpProjectId' => $wp_project_id, 
-				'projectName' => $wp_project_name
+				'project_wp_id' => $wp_project_id, 
+				'project_name' => $wp_project_name
 			)
 		);
 		$result_obj = BoppLib::call_post('projects', $request_params);
@@ -267,225 +267,63 @@ class BoppLib {
 	 * @return object
 	 */
 	public static function update_project($id, $params) {
-
 		$bopp= BoppLib::get_project($id);
 
-/*		if ($params['wp_project_name'] == null) {
-			$params['wp_project_name'] = $bopp->project_name;
-		}
-
-		if ($params['wp_project_slogan'] == null) {
-			$params['wp_project_slogan'] = $bopp->project_slogan;
-		}
-
-		if ($params['wp_project_description'] == null) {
-			$params['wp_project_description'] = $bopp->project_description;
-		}
-
-		if ($params['wp_project_video'] == null) {
-			$params['wp_project_video'] = $bopp->project_video;
-		}
-
-		if ($params['wp_project_category'] == null) {
-			$params['wp_project_category'] = $bopp->project_category;
-		}
-
-		if ($params['wp_project_business_sector'] == null) {
-			$params['wp_project_business_sector'] = $bopp->project_business_sector;
-		}
-
-		if ($params['wp_project_funding_type'] == null) {
-			$params['wp_project_funding_type'] = $bopp->project_funding_type;
-		}
-
-		if ($params['wp_project_funding_duration'] == null) {
-			$params['wp_project_funding_duration'] = $bopp->project_funding_duration;
-		}
-
-		if ($params['wp_project_return_on_investment'] == null) {
-			$params['wp_project_return_on_investment'] = $bopp->project_return_on_investment;
-		}
-
-		if ($params['wp_project_investor_benefit'] == null) {
-			$params['wp_project_investor_benefit'] = $bopp->project_investor_benefit;
-		}
-
-		if ($params['wp_project_summary'] == null) {
-			$params['wp_project_summary'] = $bopp->project_summary;
-		}
-
-		if ($params['wp_project_economy_excerpt'] == null) {
-			$params['wp_project_economy_excerpt'] = $bopp->project_economy_excerpt;
-		}
-
-		if ($params['wp_project_social_excerpt'] == null) {
-			$params['wp_project_social_excerpt'] = $bopp->project_social_excerpt;
-		}
-
-		if ($params['wp_project_environment_excerpt'] == null) {
-			$params['wp_project_environment_excerpt'] = $bopp->project_environment_excerpt;
-		}
-
-		if ($params['wp_project_mission'] == null) {
-			$params['wp_project_mission'] = $bopp->project_mission;
-		}
-
-		if ($params['wp_project_economy'] == null) {
-			$params['wp_project_economy'] = $bopp->project_economy;
-		}
-
-		if ($params['wp_project_social'] == null) {
-			$params['wp_project_social'] = $bopp->project_social;
-		}
-
-		if ($params['wp_project_environment'] == null) {
-			$params['wp_project_environment'] = $bopp->project_environment;
-		}
-
-		if ($params['wp_project_measure_performance'] == null) {
-			$params['wp_project_measure_performance'] = $bopp->project_measure_performance;
-		}
-
-		if ($params['wp_project_good_point'] == null) {
-			$params['wp_project_good_point'] = $bopp->project_good_point;
-		}
-
-		if ($bopp->project_context_excerpt != null) {
-			$params['wp_project_context_excerpt'] = $bopp->project_context_excerpt;
-		}
-
-		if ($bopp->project_market_excerpt != null) {
-			$params['wp_project_market_excerpt'] = $bopp->project_market_excerpt;
-		}
-
-		if ($bopp->project_context != null) {
-			$params['wp_project_context'] = $bopp->project_context;
-		}
-
-		if ($bopp->project_market != null) {
-			$params['wp_project_market'] = $bopp->project_market;
-		}
-
-		if ($params['wp_project_worth_offer'] == null) {
-			$params['wp_project_worth_offer'] = $bopp->project_worth_offer;
-		}
-
-		if ($params['wp_project_client_collaborator'] == null) {
-			$params['wp_project_client_collaborator'] = $bopp->project_client_collaborator;
-		}
-
-		if ($params['wp_project_business_core'] == null) {
-			$params['wp_project_business_core'] = $bopp->project_business_core;
-		}
-
-		if ($params['wp_project_income'] == null) {
-			$params['wp_project_income'] = $bopp->project_income;
-		}
-
-		if ($params['wp_project_cost'] == null) {
-			$params['wp_project_cost'] = $bopp->project_cost;
-		}
-
-		if ($params['wp_project_collaborators_canvas'] == null) {
-			$params['wp_project_collaborators_canvas'] = $bopp->project_collaborators_canvas;
-		}
-
-		if ($params['wp_project_activities_canvas'] == null) {
-			$params['wp_project_activities_canvas'] = $bopp->project_activities_canvas;
-		}
-
-		if ($params['wp_project_ressources_canvas'] == null) {
-			$params['wp_project_ressources_canvas'] = $bopp->project_ressources_canvas;
-		}
-
-		if ($params['wp_project_worth_offer_canvas'] == null) {
-			$params['wp_project_worth_offer_canvas'] = $bopp->project_worth_offer_canvas;
-		}
-
-		if ($params['wp_project_customers_relations_canvas'] == null) {
-			$params['wp_project_customers_relations_canvas'] = $bopp->project_customers_relations_canvas;
-		}
-
-		if ($params['wp_project_chain_distribution_canvas'] == null) {
-			$params['wp_project_chain_distribution_canvas'] = $bopp->project_chain_distribution_canvas;
-		}
-
-		if ($params['wp_project_clients_canvas'] == null) {
-			$params['wp_project_clients_canvas'] = $bopp->project_clients_canvas;
-		}
-
-		if ($params['wp_project_structure_canvas'] == null) {
-			$params['wp_project_structure_canvas'] = $bopp->project_structure_canvas;
-		}
-
-		if ($params['wp_project_source_income_canvas'] == null) {
-			$params['wp_project_source_income_canvas'] = $bopp->project_source_income_canvas;
-		}
-
-		if ($params['wp_project_financial_board'] == null) {
-			$params['wp_project_financial_board'] = $bopp->project_financial_board;
-		}
-
-		if ($params['wp_project_perspectives'] == null) {
-			$params['wp_project_perspectives'] = $bopp->project_perspectives;
-		}
-
-		if ($params['wp_project_other_information'] == null) {
-			$params['wp_project_other_information'] = $bopp->project_other_information;
-		}*/
-
 		$request_params = array(
-			'projects' => array(
-				'wpProjectId' => $params['wp_project_id'], 
-	            'projectName' => $params['wp_project_name'],
-	            'projectSlogan' => $params['wp_project_slogan'],
-	            'projectDescription' => $params['wp_project_description'],
-	            'projectVideo' => $params['wp_project_video'],
-	            'projectImageVideo' => $params['wp_project_image_video'],
-	            'projectImageCover' => $params['wp_project_image_cover'],
-	            'projectCategory' => $params['wp_project_category'],
-	            'projectBusinessSector' => $params['wp_project_business_sector'],
-	            'projectFundingType' => $params['wp_project_funding_type'],
-	            'projectFundingDuration' => $params['wp_project_funding_duration'],
-	            'projectReturnOnInvestment' => $params['wp_project_return_on_investment'],
-	            'projectInvestorBenefit' => $params['wp_project_investor_benefit'],
-	            'projectSummary' => $params['wp_project_summary'],
-	            'projectEconomyExcerpt' => $params['wp_project_economy_excerpt'],
-	            'projectSocialExcerpt' => $params['wp_project_social_excerpt'],
-	            'projectEnvironmentExcerpt' => $params['wp_project_environment_excerpt'],
-	            'projectMission' => $params['wp_project_mission'],
-	            'projectEconomy' => $params['wp_project_economy'],
-	            'projectSocial' => $params['wp_project_social'],
-	            'projectEnvironment' => $params['wp_project_environment'],
-	            'projectMeasurePerformance' => $params['wp_project_measure_performance'],
-	            'projectGoodPoint' => $params['wp_project_good_point'],
-	            'projectContextExcerpt' => $params['wp_project_context_excerpt'],
-	            'projectMarketExcerpt' => $params['wp_project_market_excerpt'],
-	            'projectContext' => $params['wp_project_context'],
-	            'projectMarket' => $params['wp_project_market'],
-	            'projectWorthOffer' => $params['wp_project_worth_offer'],
-	            'projectClientCollaborator' => $params['wp_project_client_collaborator'],
-	            'projectBusinessCore' => $params['wp_project_business_core'],
-	            'projectIncome' => $params['wp_project_income'],
-	            'projectCost' => $params['wp_project_cost'],
-	            'projectCollaboratorsCanvas' => $params['wp_project_collaborators_canvas'],
-	            'projectActivitiesCanvas' => $params['wp_project_activities_canvas'],
-	            'projectRessourcesCanvas' => $params['wp_project_ressources_canvas'],
-	            'projectWorthOfferCanvas' => $params['wp_project_worth_offer_canvas'],
-	            'projectCustomersRelationsCanvas' => $params['wp_project_customers_relations_canvas'],
-	            'projectChainDistributionsCanvas' => $params['wp_project_chain_distribution_canvas'],
-	            'projectClientsCanvas' => $params['wp_project_clients_canvas'],
-	            'projectCostStructureCanvas' => $params['wp_project_structure_canvas'],
-	            'projectSourceOfIncomeCanvas' => $params['wp_project_source_income_canvas'],
-	            'projectFinancialBoard' => $params['wp_project_financial_board'],
-	            'projectPerspectives' => $params['wp_project_perspectives'],
-	            'projectOtherInformation' => $params['wp_project_other_information']
-			)
+			'project' => array()
 		);
-		//var_dump($request_params);
+		$asso_tab = array(
+				'project_wp_id' => 'wp_project_id',
+				'project_name' => 'wp_project_name',
+	            'project_slogan' => 'wp_project_slogan',
+	            'project_description' => 'wp_project_description',
+	            'project_video_url' => 'wp_project_video',
+	            'project_image_url' => 'wp_project_image_cover',
+	            'project_category' => 'wp_project_category',
+	            'project_business_sector' => 'wp_project_business_sector',
+	            'project_funding_type' => 'wp_project_funding_type',
+	            'project_funding_duration' => 'wp_project_funding_duration',
+	            'project_return_on_investment' => 'wp_project_return_on_investment',
+	            'project_investor_benefit' => 'wp_project_investor_benefit',
+	            'project_summary' => 'wp_project_summary',
+	            'project_economy_excerpt' => 'wp_project_economy_excerpt',
+	            'project_social_excerpt' => 'wp_project_social_excerpt',
+	            'project_environment_excerpt' => 'wp_project_environment_excerpt',
+	            'project_mission' => 'wp_project_mission',
+	            'project_economy' => 'wp_project_economy',
+	            'project_social' => 'wp_project_social',
+	            'project_environment' => 'wp_project_environment',
+	            'project_measure_performance' => 'wp_project_measure_performance',
+	            'project_good_point' => 'wp_project_good_point',
+	            'project_context_excerpt' => 'wp_project_context_excerpt',
+	            'project_market_excerpt' => 'wp_project_market_excerpt',
+	            'project_context' => 'wp_project_context',
+	            'project_market' => 'wp_project_market',
+	            'project_worth_offer' => 'wp_project_worth_offer',
+	            'project_client_collaborator' => 'wp_project_client_collaborator',
+	            'project_business_core' => 'wp_project_business_core',
+	            'project_income' => 'wp_project_income',
+	            'project_cost' => 'wp_project_cost',
+	            'project_collaborators_canvas' => 'wp_project_collaborators_canvas',
+	            'project_activities_canvas' => 'wp_project_activities_canvas',
+	            'project_ressources_canvas' => 'wp_project_ressources_canvas',
+	            'project_worth_offer_canvas' => 'wp_project_worth_offer_canvas',
+	            'project_customers_relations_canvas' => 'wp_project_customers_relations_canvas',
+	            'project_chain_distribution_canvas' => 'wp_project_chain_distribution_canvas',
+	            'project_clients_canvas' => 'wp_project_clients_canvas',
+	            'project_cost_structure_canvas' => 'wp_project_structure_canvas',
+	            'project_source_of_income_canvas' => 'wp_project_source_income_canvas',
+	            'project_financial_board' => 'wp_project_financial_board',
+	            'project_perspectives' => 'wp_project_perspectives',
+	            'project_other_information' => 'wp_project_other_information'
+		);
+		foreach ($asso_tab as $key => $value) {
+			if ($params[$value] != null) $request_params['project'][$key] = $params[$value];
+		}
 		$result_obj = BoppLib::call_patch('projects/' . $id, $request_params);
 		return $result_obj;
 	}
+
 
 //FIN GESTION PROJETS
 //******************************************************************************************//
@@ -499,9 +337,9 @@ class BoppLib {
 	 */
 	public static function add_role($title, $slug) {
 		$request_params = array(
-			'roles' => array(
-				'roleName' => $title, 
-				'roleSlug' => $slug
+			'role' => array(
+				'role_name' => $title, 
+				'role_slug' => $slug
 			)
 		);
 		$result_obj = BoppLib::call_post('roles', $request_params);
@@ -534,9 +372,9 @@ class BoppLib {
 	 */
 	public static function link_user_to_project($api_project_id, $api_user_id, $api_role_slug) {
 		$request_params = array(
-			'projectsUsers' => array(
-				'users' => $api_user_id, 
-				'roles' => $api_role_slug
+			'project_management' => array(
+				'boppUaser' => $api_user_id, 
+				'boppRole' => $api_role_slug
 			)
 		);
 		$result_obj = BoppLib::call_post('projects/'.$api_project_id.'/members', $request_params);
