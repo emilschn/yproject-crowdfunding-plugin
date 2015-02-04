9c4d0e1fa41211dc852451bae182c2ce078c2b11
Emilien Schneider <emilien.schneider@gmail.com>
2015-02-04 11:10:46 +0100

    Eviter perte département si création projet incomplète Signed-off-by: Emilien Schneider <emilien.schneider@gmail.com>

--- includes/shortcode-submit.php
+++ includes/shortcode-submit.php
@@ -185,109 +185,117 @@ function ypcf_shortcode_submit_field_complex($atts, $content = '') {
 add_shortcode('yproject_crowdfunding_field_complex', 'ypcf_shortcode_submit_field_complex');
 
 function ypcf_shortcode_submit_field_location($atts, $content = '') {
-    return '<select id="location" name="location">
-	<option>01 Ain</option>
-      <option>02 Aisne</option>
-      <option>03 Allier</option>
-      <option>04 Alpes-de-Haute-Provence</option>
-      <option>05 Hautes-Alpes</option>
-      <option>06 Alpes-Maritimes</option>
-      <option>07 Ard&egraveche</option>
-      <option>08 Ardennes</option>
-      <option>09 Ari&egravege</option>
-      <option>10 Aube</option>
-      <option>11 Aude</option>
-      <option>12 Aveyron</option>
-      <option>13 Bouches-du-Rh&ocircne</option>
-      <option>14 Calvados</option>
-      <option>15 Cantal</option>
-      <option>16 Charente</option>
-      <option>17 Charente-Maritime</option>
-      <option>18 Cher</option>
-      <option>19 Corr&egraveze</option>
-      <option>2A Corse-du-Sud</option>
-      <option>2B Haute-Corse</option>
-      <option>21 C&ocircte-d\'Or</option>
-      <option>22 C&ocirctes d\'Armor</option>
-      <option>23 Creuse</option>
-      <option>24 Dordogne</option>
-      <option>25 Doubs</option>
-      <option>26 Dr&ocircme</option>
-      <option>27 Eure</option>
-      <option>28 Eure-et-Loir</option>
-      <option>29 Finist&egravere</option>
-      <option>30 Gard</option>
-      <option>31 Haute-Garonne</option>
-      <option>32 Gers</option>
-      <option>33 Gironde</option>
-      <option>34 H&eacuterault</option>
-      <option>35 Ille-et-Vilaine</option>
-      <option>36 Indre</option>
-      <option>37 Indre-et-Loire</option>
-      <option>38 Is&egravere</option>
-      <option>39 Jura</option>
-      <option>40 Landes</option>
-      <option>41 Loir-et-Cher</option>
-      <option>42 Loire</option>
-      <option>43 Haute-Loire</option>
-      <option>44 Loire-Atlantique</option>
-      <option>45 Loiret</option>
-      <option>46 Lot</option>
-      <option>47 Lot-et-Garonne</option>
-      <option>48 Loz&egravere</option>
-      <option>49 Maine-et-Loire</option>
-      <option>50 Manche</option>
-      <option>51 Marne</option>
-      <option>52 Haute-Marne</option>
-      <option>53 Mayenne</option>
-      <option>54 Meurthe-et-Moselle</option>
-      <option>55 Meuse</option>
-      <option>56 Morbihan</option>
-      <option>57 Moselle</option>
-      <option>58 Ni&egravevre</option>
-      <option>59 Nord</option>
-      <option>60 Oise</option>
-      <option>61 Orne</option>
-      <option>62 Pas-de-Calais</option>
-      <option>63 Puy-de-D&ocircme</option>
-      <option>64 Pyr&eacuten&eacutees-Atlantiques</option>
-      <option>65 Hautes-Pyr&eacuten&eacutees</option>
-      <option>66 Pyr&eacuten&eacutees-Orientales</option>
-      <option>67 Bas-Rhin</option>
-      <option>68 Haut-Rhin</option>
-      <option>69 Rh&ocircne</option>
-      <option>70 Haute-Sa&ocircne</option>
-      <option>71 Sa&ocircne-et-Loire</option>
-      <option>72 Sarthe</option>
-      <option>73 Savoie</option>
-      <option>74 Haute-Savoie</option>
-      <option>75 Paris</option>
-      <option>76 Seine-Maritime</option>
-      <option>77 Seine-et-Marne</option>
-      <option>78 Yvelines</option>
-      <option>79 Deux-S&egravevres</option>
-      <option>80 Somme</option>
-      <option>81 Tarn</option>
-      <option>82 Tarn-et-Garonne</option>
-      <option>83 Var</option>
-      <option>84 Vaucluse</option>
-      <option>85 Vend&eacutee</option>
-      <option>86 Vienne</option>
-      <option>87 Haute-Vienne</option>
-      <option>88 Vosges</option>
-      <option>89 Yonne</option>
-      <option>90 Territoire de Belfort</option>
-      <option>91 Essonne</option>
-      <option>92 Hauts-de-Seine</option>
-      <option>93 Seine-Saint-Denis</option>
-      <option>94 Val-de-Marne</option>
-      <option>95 Val-d\'Oise</option>
-      <option>971 Guadeloupe</option>
-      <option>972 Martinique</option>
-      <option>973 Guyane</option>
-      <option>974 La Réunion</option>
-      <option>976 Mayotte</option>
-    </select>';
+    $location_list = array(
+	'01' => 'Ain',
+	'02' => 'Aisne',
+	'03' => 'Allier',
+	'04' => 'Alpes-de-Haute-Provence',
+	'05' => 'Hautes-Alpes',
+	'06' => 'Alpes-Maritimes',
+	'07' => 'Ard&egrave;che',
+	'08' => 'Ardennes',
+	'09' => 'Ari&egrave;ge',
+	'10' => 'Aube',
+	'11' => 'Aude',
+	'12' => 'Aveyron',
+	'13' => 'Bouches-du-Rh&ocirc;ne',
+	'14' => 'Calvados',
+	'15' => 'Cantal',
+	'16' => 'Charente',
+	'17' => 'Charente-Maritime',
+	'18' => 'Cher',
+	'19' => 'Corr&egrave;ze',
+	'2A' => 'Corse-du-Sud',
+	'2B' => 'Haute-Corse',
+	'21' => 'C&ocirc;te-d\'Or',
+	'22' => 'C&ocirc;tes d\'Armor',
+	'23' => 'Creuse',
+	'24' => 'Dordogne',
+	'25' => 'Doubs',
+	'26' => 'Dr&ocirc;me',
+	'27' => 'Eure',
+	'28' => 'Eure-et-Loir',
+	'29' => 'Finist&egravere',
+	'30' => 'Gard',
+	'31' => 'Haute-Garonne',
+	'32' => 'Gers',
+	'33' => 'Gironde',
+	'34' => 'H&eacute;rault',
+	'35' => 'Ille-et-Vilaine',
+	'36' => 'Indre',
+	'37' => 'Indre-et-Loire',
+	'38' => 'Is&egrave;re',
+	'39' => 'Jura',
+	'40' => 'Landes',
+	'41' => 'Loir-et-Cher',
+	'42' => 'Loire',
+	'43' => 'Haute-Loire',
+	'44' => 'Loire-Atlantique',
+	'45' => 'Loiret',
+	'46' => 'Lot',
+	'47' => 'Lot-et-Garonne',
+	'48' => 'Loz&egrave;re',
+	'49' => 'Maine-et-Loire',
+	'50' => 'Manche',
+	'51' => 'Marne',
+	'52' => 'Haute-Marne',
+	'53' => 'Mayenne',
+	'54' => 'Meurthe-et-Moselle',
+	'55' => 'Meuse',
+	'56' => 'Morbihan',
+	'57' => 'Moselle',
+	'58' => 'Ni&egrave;vre',
+	'59' => 'Nord',
+	'60' => 'Oise',
+	'61' => 'Orne',
+	'62' => 'Pas-de-Calais',
+	'63' => 'Puy-de-D&ocirc;me',
+	'64' => 'Pyr&eacute;n&eacute;es-Atlantiques',
+	'65' => 'Hautes-Pyr&eacuten&eacute;es',
+	'66' => 'Pyr&eacute;n&eacute;es-Orientales',
+	'67' => 'Bas-Rhin',
+	'68' => 'Haut-Rhin',
+	'69' => 'Rh&ocirc;ne',
+	'70' => 'Haute-Sa&ocirc;ne',
+	'71' => 'Sa&ocirc;ne-et-Loire',
+	'72' => 'Sarthe',
+	'73' => 'Savoie',
+	'74' => 'Haute-Savoie',
+	'75' => 'Paris',
+	'76' => 'Seine-Maritime',
+	'77' => 'Seine-et-Marne',
+	'78' => 'Yvelines',
+	'79' => 'Deux-S&egrave;vres',
+	'80' => 'Somme',
+	'81' => 'Tarn',
+	'82' => 'Tarn-et-Garonne',
+	'83' => 'Var',
+	'84' => 'Vaucluse',
+	'85' => 'Vend&eacute;e',
+	'86' => 'Vienne',
+	'87' => 'Haute-Vienne',
+	'88' => 'Vosges',
+	'89' => 'Yonne',
+	'90' => 'Territoire de Belfort',
+	'91' => 'Essonne',
+	'92' => 'Hauts-de-Seine',
+	'93' => 'Seine-Saint-Denis',
+	'94' => 'Val-de-Marne',
+	'95' => 'Val-d\'Oise',
+	'971' => 'Guadeloupe',
+	'972' => 'Martinique',
+	'973' => 'Guyane',
+	'974' => 'La R&eacute;union',
+	'976' => 'Mayotte',
+    );
+    $buffer = '<select id="location" name="location">';
+    foreach ($location_list as $location_key => $location_name) {
+	$location_str = $location_key.' '.$location_name;
+	$selection_str = (isset($_POST['location']) && $_POST['location'] == $location_str) ? ' selected="selected"' : '';
+	$buffer .= '<option'.$selection_str.'>'.$location_str.'</option>';
+    }
+    $buffer .= '</select>';
+    return $buffer;
 }
 add_shortcode('yproject_crowdfunding_field_location', 'ypcf_shortcode_submit_field_location');
 
