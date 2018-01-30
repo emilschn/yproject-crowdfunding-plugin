c882474f08424196df7f216e5b2f3c6e9772b656
emilschn <emilien.schneider@poucr.net>
2018-01-30 12:08:32 +0100

    XML projets : ne pas prendre les projets masqués

--- includes/control/cron.php
+++ includes/control/cron.php
@@ -90,67 +90,71 @@ class WDGCronActions {
 		while (have_posts()): the_post();
 			global $post;
 			$campaign = atcf_get_campaign( $post );
-			$organization = $campaign->get_organization();
-			$organization_obj = new WDGOrganization( $organization->wpref );
+			if ( !$campaign->is_hidden() ) {
 			
-			//*****************
-			//Formatage pour RSS
-			$buffer_rss .= '<item>' . "\n";
-			$buffer_rss .= '<title><![CDATA['.$campaign->data->post_title.']]></title>' . "\n";
-			$buffer_rss .= '<description><![CDATA['.html_entity_decode($campaign->summary()).']]></description>' . "\n";
-			$buffer_rss .= '<pubDate>'.$campaign->begin_collecte_date('Y-m-d').'</pubDate>' . "\n";
-			$buffer_rss .= '<link>'.get_permalink($campaign->ID).'</link>' . "\n";
-			$buffer_rss .= '</item>' . "\n";
-			//*****************
-			
-			
-			//*****************
-			//Formatage pour partenaires
-			$buffer_partners .= '<projet>' . "\n";
-			
-			//Toutes les données pour TNP
-			$buffer_partners .= '<reference_partenaire>099</reference_partenaire>' . "\n"; //TNP :: (010,011 => 039 pour le DON, 040,041 => 069 pour le prêt, 070, 071 => 099 pour l'investissement SANS rentrer dans la subdivision)
-			$buffer_partners .= '<date_export>'.$current_date.'</date_export>' . "\n"; //TNP :: YYYY-MM-DD
-			$buffer_partners .= '<reference_projet>'.$campaign->ID.'</reference_projet>' . "\n"; //TNP :: ref unique interne
-			
-			//TNP :: impacts : min 1, max 2
-			$buffer_partners .= '<impact_social>non</impact_social>' . "\n";
-			$buffer_partners .= '<impact_environnemental>non</impact_environnemental>' . "\n";
-			$buffer_partners .= '<impact_culturel>non</impact_culturel>' . "\n";
-			$buffer_partners .= '<impact_eco>oui</impact_eco>' . "\n";
-			
-			$buffer_partners .= '<mots_cles_nomenclature_operateur></mots_cles_nomenclature_operateur>' . "\n"; //TNP :: Mots-clés TODO
-			$buffer_partners .= '<mode_financement>ROY</mode_financement>' . "\n"; //TNP :: Mode de financement (DON, DOC, PRE, PRR, ACT, OBL) - invention ROY
-			$buffer_partners .= '<type_porteur_projet>ENT</type_porteur_projet>' . "\n"; //TNP :: Statut du PP (ENT, ASS, PAR, COL)
-			$buffer_partners .= '<qualif_ESS>non</qualif_ESS>' . "\n"; //TNP :: Qualification ESS du porteur projet
-			$buffer_partners .= '<code_postal>' .$organization_obj->get_postal_code(). '</code_postal>' . "\n";
-			$buffer_partners .= '<ville>' .$organization_obj->get_city(). '</ville>' . "\n";
-			
-			$buffer_partners .= '<titre><![CDATA['.$campaign->data->post_title.']]></titre>' . "\n"; //TNP
-			$buffer_partners .= '<description><![CDATA['.html_entity_decode($campaign->summary()).']]></description>' . "\n"; //TNP
-			$description_complete = html_entity_decode( $campaign->description() );
-			$buffer_partners .= '<description_complete><![CDATA['.apply_filters( 'the_content', $description_complete ).']]></description_complete>' . "\n"; //Info durable
-			$buffer_partners .= '<url>'.get_permalink($campaign->ID).'</url>' . "\n"; //TNP
-			$buffer_partners .= '<url_photo>'.$campaign->get_home_picture_src().'</url_photo>' . "\n"; //TNP
-			$buffer_partners .= '<date_debut_collecte>'.$campaign->begin_collecte_date('Y-m-d').'</date_debut_collecte>' . "\n"; //TNP :: YYYY-MM-DD 
-			$buffer_partners .= '<date_fin_collecte>'.$campaign->end_date('Y-m-d').'</date_fin_collecte>' . "\n"; //TNP :: YYYY-MM-DD 
-			$buffer_partners .= '<nb_jours_restants>'.$campaign->days_remaining().'</nb_jours_restants>' . "\n";
-			$buffer_partners .= '<montant_recherche>'.$campaign->minimum_goal(false).'</montant_recherche>' . "\n"; //TNP :: Somme recherchée
-			$buffer_partners .= '<montant_collecte>'.$campaign->current_amount(false).'</montant_collecte>' . "\n"; //TNP :: Somme collectée
-			
-			//Données complémentaires pour BeCrowd
-			$buffer_partners .= '<type>royalty</type>' . "\n";
-			$buffer_partners .= '<pourcentage>'.$campaign->percent_completed(false).'</pourcentage>' . "\n";
-			$buffer_partners .= '<nb>'.$campaign->backers_count().'</nb>' . "\n";
-			$buffer_partners .= '<url_video><![CDATA['.$campaign->video().']]></url_video>' . "\n";
+				$organization = $campaign->get_organization();
+				$organization_obj = new WDGOrganization( $organization->wpref );
 
-			//Données complémentaires pour mon petit voisinage
-			$buffer_partners .= '<latitude>'.$organization_obj->get_latitude().'</latitude>' . "\n";
-			$buffer_partners .= '<longitude>'.$organization_obj->get_longitude().'</longitude>' . "\n";
+				//*****************
+				//Formatage pour RSS
+				$buffer_rss .= '<item>' . "\n";
+				$buffer_rss .= '<title><![CDATA['.$campaign->data->post_title.']]></title>' . "\n";
+				$buffer_rss .= '<description><![CDATA['.html_entity_decode($campaign->summary()).']]></description>' . "\n";
+				$buffer_rss .= '<pubDate>'.$campaign->begin_collecte_date('Y-m-d').'</pubDate>' . "\n";
+				$buffer_rss .= '<link>'.get_permalink($campaign->ID).'</link>' . "\n";
+				$buffer_rss .= '</item>' . "\n";
+				//*****************
 
-		
-			$buffer_partners .= '</projet>' . "\n";
-			//*****************
+
+				//*****************
+				//Formatage pour partenaires
+				$buffer_partners .= '<projet>' . "\n";
+
+				//Toutes les données pour TNP
+				$buffer_partners .= '<reference_partenaire>099</reference_partenaire>' . "\n"; //TNP :: (010,011 => 039 pour le DON, 040,041 => 069 pour le prêt, 070, 071 => 099 pour l'investissement SANS rentrer dans la subdivision)
+				$buffer_partners .= '<date_export>'.$current_date.'</date_export>' . "\n"; //TNP :: YYYY-MM-DD
+				$buffer_partners .= '<reference_projet>'.$campaign->ID.'</reference_projet>' . "\n"; //TNP :: ref unique interne
+
+				//TNP :: impacts : min 1, max 2
+				$buffer_partners .= '<impact_social>non</impact_social>' . "\n";
+				$buffer_partners .= '<impact_environnemental>non</impact_environnemental>' . "\n";
+				$buffer_partners .= '<impact_culturel>non</impact_culturel>' . "\n";
+				$buffer_partners .= '<impact_eco>oui</impact_eco>' . "\n";
+
+				$buffer_partners .= '<mots_cles_nomenclature_operateur></mots_cles_nomenclature_operateur>' . "\n"; //TNP :: Mots-clés TODO
+				$buffer_partners .= '<mode_financement>ROY</mode_financement>' . "\n"; //TNP :: Mode de financement (DON, DOC, PRE, PRR, ACT, OBL) - invention ROY
+				$buffer_partners .= '<type_porteur_projet>ENT</type_porteur_projet>' . "\n"; //TNP :: Statut du PP (ENT, ASS, PAR, COL)
+				$buffer_partners .= '<qualif_ESS>non</qualif_ESS>' . "\n"; //TNP :: Qualification ESS du porteur projet
+				$buffer_partners .= '<code_postal>' .$organization_obj->get_postal_code(). '</code_postal>' . "\n";
+				$buffer_partners .= '<ville>' .$organization_obj->get_city(). '</ville>' . "\n";
+
+				$buffer_partners .= '<titre><![CDATA['.$campaign->data->post_title.']]></titre>' . "\n"; //TNP
+				$buffer_partners .= '<description><![CDATA['.html_entity_decode($campaign->summary()).']]></description>' . "\n"; //TNP
+				$description_complete = html_entity_decode( $campaign->description() );
+				$buffer_partners .= '<description_complete><![CDATA['.apply_filters( 'the_content', $description_complete ).']]></description_complete>' . "\n"; //Info durable
+				$buffer_partners .= '<url>'.get_permalink($campaign->ID).'</url>' . "\n"; //TNP
+				$buffer_partners .= '<url_photo>'.$campaign->get_home_picture_src().'</url_photo>' . "\n"; //TNP
+				$buffer_partners .= '<date_debut_collecte>'.$campaign->begin_collecte_date('Y-m-d').'</date_debut_collecte>' . "\n"; //TNP :: YYYY-MM-DD 
+				$buffer_partners .= '<date_fin_collecte>'.$campaign->end_date('Y-m-d').'</date_fin_collecte>' . "\n"; //TNP :: YYYY-MM-DD 
+				$buffer_partners .= '<nb_jours_restants>'.$campaign->days_remaining().'</nb_jours_restants>' . "\n";
+				$buffer_partners .= '<montant_recherche>'.$campaign->minimum_goal(false).'</montant_recherche>' . "\n"; //TNP :: Somme recherchée
+				$buffer_partners .= '<montant_collecte>'.$campaign->current_amount(false).'</montant_collecte>' . "\n"; //TNP :: Somme collectée
+
+				//Données complémentaires pour BeCrowd
+				$buffer_partners .= '<type>royalty</type>' . "\n";
+				$buffer_partners .= '<pourcentage>'.$campaign->percent_completed(false).'</pourcentage>' . "\n";
+				$buffer_partners .= '<nb>'.$campaign->backers_count().'</nb>' . "\n";
+				$buffer_partners .= '<url_video><![CDATA['.$campaign->video().']]></url_video>' . "\n";
+
+				//Données complémentaires pour mon petit voisinage
+				$buffer_partners .= '<latitude>'.$organization_obj->get_latitude().'</latitude>' . "\n";
+				$buffer_partners .= '<longitude>'.$organization_obj->get_longitude().'</longitude>' . "\n";
+
+
+				$buffer_partners .= '</projet>' . "\n";
+				//*****************
+			
+			}
 		endwhile;
 		wp_reset_query();
 		
