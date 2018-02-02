fe52fadf503c2fcc9627e489bbf497c4163d0f1e
emilschn <emilien.schneider@poucr.net>
2018-02-02 14:51:29 +0100

    Modèles de contrats : ajout gestion

--- crowdfunding.php
+++ crowdfunding.php
@@ -115,6 +115,7 @@ final class ATCF_CrowdFunding {
 		require( $this->includes_dir . 'data/wdgwprest/wdgwprest-entities/wdgwprest-declaration.php' );
 		require( $this->includes_dir . 'data/wdgwprest/wdgwprest-entities/wdgwprest-roi.php' );
 		require( $this->includes_dir . 'data/wdgwprest/wdgwprest-entities/wdgwprest-bankinfo.php' );
+		require( $this->includes_dir . 'data/wdgwprest/wdgwprest-entities/wdgwprest-contract-model.php' );
 		
 		require( $this->includes_dir . 'control/social/FacebookApp/autoload.php' );
 
