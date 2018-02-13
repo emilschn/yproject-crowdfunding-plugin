1ae247d84311826f650e919f4b5348d3412ba398
emilschn <emilien.schneider@poucr.net>
2018-02-13 10:13:37 +0100

    TBPP > Envoi mail : correction récupération id user

--- includes/control/post.php
+++ includes/control/post.php
@@ -83,24 +83,27 @@ class WDGPostActions {
         $campaign_id = sanitize_text_field( filter_input( INPUT_POST, 'campaign_id' ) );
 		$post_campaign = get_post( $campaign_id );
 		$author_user = get_user_by( 'ID', $post_campaign->post_author );
-        $mail_title = sanitize_text_field(filter_input(INPUT_POST,'mail_title'));
+        $mail_title = sanitize_text_field( filter_input( INPUT_POST, 'mail_title' ) );
         $mail_content = nl2br( filter_input( INPUT_POST, 'mail_content' ) );
 		$mail_recipients = explode( ',', filter_input( INPUT_POST, 'mail_recipients' ) );
 		
 		global $wpdb;
         $table_vote = $wpdb->prefix . "ypcf_project_votes";
         $list_user_voters = $wpdb->get_results( "SELECT user_id, invest_sum FROM ".$table_vote." WHERE post_id = ".$campaign_id." AND validate_project = 1", OBJECT_K);
-
+		
         foreach ( $mail_recipients as $id_user ) {
-			if ( is_int( $id_user ) ) {
+			if ( is_numeric( $id_user ) ) {
 				//TODO : Re-vérifier si l'utilisateur peut bien envoyer à la personne (vérifier si dans la liste des suiveurs/votants/investisseurs)
 				$user = get_userdata( intval( $id_user ) );
 				$to = $user->user_email;
-				$user_data= array(
+				$user_data = array(
 					'userfirstname'	=> $user->first_name,
 					'userlastname'	=> $user->last_name,
-					'investwish'	=> $list_user_voters[ $id_user ]->invest_sum
+					'investwish'	=> 0
 				);
+				if ( isset( $list_user_voters[ $id_user ] ) && isset( $list_user_voters[ $id_user ]->invest_sum ) ) {
+					$user_data[ 'investwish' ] = $list_user_voters[ $id_user ]->invest_sum;
+				}
 
 				$this_mail_content = WDGFormProjects::build_mail_text( $mail_content, $mail_title, $campaign_id, $user_data );
 
