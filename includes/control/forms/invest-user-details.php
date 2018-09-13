7f4d6521ad170cd2772de75c62eac118f0798505
emilschn <emilien.schneider@poucr.net>
2018-09-13 11:45:41 +0200

    Formulaires : Utilisateur : intégration d'une liste déroulante pour les pays

--- includes/control/forms/invest-user-details.php
+++ includes/control/forms/invest-user-details.php
@@ -158,11 +158,13 @@ class WDG_Form_Invest_User_Details extends WDG_Form {
 		);
 
 		$this->addField(
-			'text',
+			'select',
 			'country',
 			__( "Pays *", 'yproject' ),
 			WDG_Form_Invest_User_Details::$field_group_user_info,
-			$WDGUser->get_country()
+			$WDGUser->get_country( 'iso2' ),
+			FALSE,
+			$country_list
 		);
 
 		$this->addField(
