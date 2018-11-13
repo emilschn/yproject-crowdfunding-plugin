c098e0051abed0b893f64115a245f596405ecac4
emilschn <emilien.schneider@poucr.net>
2018-11-13 11:40:19 +0100

    Campagnes : Actualités : URL sous les-projets et prefixee par le nom du projet

--- includes/data/campaigns.php
+++ includes/data/campaigns.php
@@ -43,8 +43,8 @@ class ATCF_Campaigns {
 	 */
 	function setup() {
 		define( 'EDD_SLUG', 'campaigns' );
-//		add_action( 'init', array( $this, 'campaigns_rewrite_news_links' ) );
-//		add_filter( 'pre_post_link', array( $this, 'campaigns_news_links' ), 1, 3 );
+		add_action( 'init', array( $this, 'campaigns_rewrite_news_links' ) );
+		add_filter( 'pre_post_link', array( $this, 'campaigns_news_links' ), 1, 3 );
 		
 		add_filter( 'post_type_link', array( $this, 'campaigns_type_links' ), 1, 3 );
 		add_action( 'pre_get_posts', array( $this, 'campaigns_type_get_posts' ) );
@@ -84,27 +84,17 @@ class ATCF_Campaigns {
 		do_action( 'atcf_campaigns_actions_admin' );
 	}
 	
-	/*function campaigns_rewrite_news_links() {
+	function campaigns_rewrite_news_links() {
 		flush_rewrite_rules();
-		add_rewrite_rule( '(.+?)/actualites/(.+?)', 'index.php?pagename=$matches[2]', 'top' );
+		add_rewrite_rule( '^les-projets/([^/]+)/?', 'index.php?name=$matches[1]', 'top' );
 	}
 	
 	function campaigns_news_links( $permalink, $post, $leavename ) {
 		if ( $post->post_type === 'post' ) {
-			$post_categories = get_the_category( $post );
-			$post_category = $post_categories[ 0 ];
-			$post_category_name = $post_category->name;
-			$name_exploded = explode( 'cat', $post_category_name );
-			if ( count( $name_exploded ) > 1 ) {
-				$campaign_id = $name_exploded[1];
-				if ( !empty( $campaign_id ) ) {
-					$campaign_post = get_post( $campaign_id );
-					return $campaign_post->post_name. '/actualites/' .$post->post_name. '/';
-				}
-			}
+			return '/les-projets/' .$post->post_name. '/';
 		}
 		return $permalink;
-	}*/
+	}
 
 	/**
 	* Réécritures des liens des campagnes
