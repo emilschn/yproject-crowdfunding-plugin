e4793440f4e7a398c4b25daf3a16c046de31aeee
emilschn <emilien.schneider@poucr.net>
2018-11-15 16:06:55 +0100

    Formulaire ajout chèque TBPP : ajout champs utilisateur

--- includes/control/forms/dashboard-add-check.php
+++ includes/control/forms/dashboard-add-check.php
@@ -5,6 +5,7 @@ class WDG_Form_Dashboard_Add_Check extends WDG_Form {
 	
 	public static $field_group_hidden = 'dashboard-add-check-hidden';
 	public static $field_group_user_email = 'dashboard-add-check-user-email';
+	public static $field_group_user_info = 'dashboard-add-check-user-info';
 	
 	private $campaign_id;
 	private $user_id;
@@ -37,6 +38,98 @@ class WDG_Form_Dashboard_Add_Check extends WDG_Form {
 			'user-email',
 			__( "E-mail de l'investisseur *", 'yproject' ),
 			self::$field_group_user_email
+		);
+		
+		//**********************************************************************
+		// Données de l'investisseur : $field_group_user_info
+		$this->addField(
+			'select',
+			'gender',
+			__( "Sexe *", 'yproject' ),
+			self::$field_group_user_info,
+			'',
+			FALSE,
+			[
+				'female'	=> __( "une femme", 'yproject' ),
+				'male'		=> __( "un homme", 'yproject' )
+			]
+		);
+		
+		$this->addField(
+			'text',
+			'firstname',
+			__( "Pr&eacute;nom *", 'yproject' ),
+			self::$field_group_user_info
+		);
+		
+		$this->addField(
+			'text',
+			'lastname',
+			__( "Nom *", 'yproject' ),
+			self::$field_group_user_info
+		);
+
+		$this->addField(
+			'date',
+			'birthday',
+			__( "Date de naissance *", 'yproject' ),
+			self::$field_group_user_info
+		);
+
+		$this->addField(
+			'text',
+			'birthplace',
+			__( "Ville de naissance *", 'yproject' ),
+			self::$field_group_user_info
+		);
+
+		global $country_list;
+		$this->addField(
+			'select',
+			'nationality',
+			__( "Nationalit&eacute; *", 'yproject' ),
+			self::$field_group_user_info,
+			'',
+			FALSE,
+			$country_list
+		);
+
+		$this->addField(
+			'text',
+			'address',
+			__( "Adresse *", 'yproject' ),
+			self::$field_group_user_info
+		);
+
+		$this->addField(
+			'text',
+			'postal_code',
+			__( "Code postal *", 'yproject' ),
+			self::$field_group_user_info
+		);
+
+		$this->addField(
+			'text',
+			'city',
+			__( "Ville *", 'yproject' ),
+			self::$field_group_user_info
+		);
+
+		$this->addField(
+			'select',
+			'country',
+			__( "Pays *", 'yproject' ),
+			self::$field_group_user_info,
+			'',
+			FALSE,
+			$country_list
+		);
+
+		$this->addField(
+			'text',
+			'phone_number',
+			__( "Num&eacute;ro de t&eacute;l&eacute;phone", 'yproject' ),
+			self::$field_group_user_info
 		);
 		
 	}
