16cb3994f0c155f09a1b7f0dd2c06061c3f272a9
emilschn <emilien.schneider@poucr.net>
2019-04-19 17:49:26 +0200

    Nouveau formulaire d'ajout de chèque : ajout de champs

--- includes/control/forms/dashboard-add-check.php
+++ includes/control/forms/dashboard-add-check.php
@@ -6,6 +6,9 @@ class WDG_Form_Dashboard_Add_Check extends WDG_Form {
 	public static $field_group_hidden = 'dashboard-add-check-hidden';
 	public static $field_group_user_email = 'dashboard-add-check-user-email';
 	public static $field_group_user_info = 'dashboard-add-check-user-info';
+	public static $field_group_orga_select = 'dashboard-add-check-orga-select';
+	public static $field_group_orga_info = 'dashboard-add-check-orga-info';
+	public static $field_group_invest_files = 'dashboard-add-check-invest-files';
 	
 	private $campaign_id;
 	private $user_id;
@@ -33,6 +36,13 @@ class WDG_Form_Dashboard_Add_Check extends WDG_Form {
 		
 		//**********************************************************************
 		// Données de l'investisseur : $field_group_user_type
+		$this->addField(
+			'text',
+			'invest-amount',
+			__( "Montant de l'investissement *", 'yproject' ),
+			self::$field_group_user_email
+		);
+		
 		$this->addField(
 			'text',
 			'user-email',
@@ -123,6 +133,156 @@ class WDG_Form_Dashboard_Add_Check extends WDG_Form {
 			'',
 			FALSE,
 			$country_list
+		);
+		
+		$this->addField(
+			'select',
+			'user_type',
+			__( "Souhaite investir... *", 'yproject' ),
+			self::$field_group_user_info,
+			'',
+			FALSE,
+			[
+				''		=> "",
+				'user'	=> __( "En son nom (personne physique)", 'yproject' ),
+				'orga'	=> __( "En tant qu'organisation (personne morale)", 'yproject' )
+			]
+		);
+		
+		//**********************************************************************
+		// Sélection de l'organisation : $field_group_orga_select
+		$this->addField(
+			'select',
+			'orga-id',
+			__( "Au nom de", 'yproject' ),
+			self::$field_group_orga_select,
+			$_SESSION[ 'orga_id' ],
+			FALSE,
+			[
+				''			=> "",
+				'new-orga'	=> __( "Nouvelle organisation", 'yproject' )
+			]
+		);
+		
+		//**********************************************************************
+		// Informations de l'orga : $field_group_orga_info
+		$this->addField(
+			'text',
+			'org_name',
+			__( "D&eacute;nomination sociale *", 'yproject' ),
+			self::$field_group_orga_info
+		);
+		
+		$this->addField(
+			'text',
+			'org_email',
+			__( "E-mail de contact *", 'yproject' ),
+			self::$field_group_orga_info,
+			FALSE,
+			__( "Cette adresse doit &ecirc;tre diff&eacute;rente de celle de votre compte.", 'yproject' )
+		);
+		
+		$this->addField(
+			'text',
+			'org_website',
+			__( "Site internet *", 'yproject' ),
+			self::$field_group_orga_info,
+			FALSE
+		);
+		
+		$this->addField(
+			'text',
+			'org_legalform',
+			__( "Forme juridique *", 'yproject' ),
+			self::$field_group_orga_info
+		);
+		
+		$this->addField(
+			'text',
+			'org_idnumber',
+			__( "Num&eacute;ro SIRET *", 'yproject' ),
+			self::$field_group_orga_info
+		);
+		
+		$this->addField(
+			'text',
+			'org_rcs',
+			__( "RCS (Ville) *", 'yproject' ),
+			self::$field_group_orga_info
+		);
+		
+		$this->addField(
+			'text',
+			'org_capital',
+			__( "Capital social (en euros) *", 'yproject' ),
+			self::$field_group_orga_info
+		);
+		
+		$this->addField(
+			'text',
+			'org_address_number',
+			__( "Num&eacute;ro *", 'yproject' ),
+			self::$field_group_orga_info
+		);
+
+		global $address_number_complements;
+		$this->addField(
+			'select',
+			'org_address_number_comp',
+			__( "Compl&eacute;ment de num&eacute;ro", 'yproject' ),
+			self::$field_group_orga_info,
+			'',
+			FALSE,
+			$address_number_complements
+		);
+			
+		
+		$this->addField(
+			'text',
+			'org_address',
+			__( "Adresse *", 'yproject' ),
+			self::$field_group_orga_info
+		);
+		
+		$this->addField(
+			'text',
+			'org_postal_code',
+			__( "Code postal *", 'yproject' ),
+			self::$field_group_orga_info
+		);
+		
+		$this->addField(
+			'text',
+			'org_city',
+			__( "Ville *", 'yproject' ),
+			self::$field_group_orga_info
+		);
+
+		global $country_list;
+		$this->addField(
+			'select',
+			'org_nationality',
+			__( "Pays *", 'yproject' ),
+			self::$field_group_orga_info,
+			FALSE,
+			FALSE,
+			$country_list
+		);
+		
+		//**********************************************************************
+		// Fichiers de l'investissement : $field_group_invest_files
+		$this->addField(
+			'file',
+			'picture-check',
+			__( "Photo du ch&egrave;que *", 'yproject' ),
+			self::$field_group_invest_files
+		);
+		
+		$this->addField(
+			'file',
+			'contract-check',
+			__( "Photos du contrat *", 'yproject' ),
+			self::$field_group_invest_files
 		);
 		
 	}
