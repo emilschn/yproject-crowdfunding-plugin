49f8a4a9831bfd89c3858e259949d5d27d2c1e74
emilschn <emilien.schneider@poucr.net>
2019-05-17 10:05:55 +0200

    Campagnes : modification requete pour la liste des projets financés pour inclure les status en post-cloture

--- includes/data/campaign.php
+++ includes/data/campaign.php
@@ -3165,7 +3165,7 @@ class ATCF_Campaign {
 	public static function get_list_funded( $nb = 0, $client = '', $include_current = false, $skip_hidden = true ) {
 		$buffer = ATCF_Campaign::get_list_finished( $nb, array( ATCF_Campaign::$campaign_status_funded, ATCF_Campaign::$campaign_status_closed ), $client, $skip_hidden );
 		if ( $include_current ) {
-			$list_current = ATCF_Campaign::get_list_current( $nb, ATCF_Campaign::$campaign_status_collecte, 'asc', $client );
+			$list_current = ATCF_Campaign::get_list_current( $nb, ATCF_Campaign::$campaign_status_collecte, 'asc', $client, FALSE );
 			foreach ( $list_current as $campaign_post ) {
 				$campaign = atcf_get_campaign( $campaign_post->ID );
 				if ( $campaign->is_funded() ) {
