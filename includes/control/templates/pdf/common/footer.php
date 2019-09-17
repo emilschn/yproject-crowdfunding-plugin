dea8283558ed0364d35c320ce491dec8d49a22d8
emilschn <emilien.schneider@poucr.net>
2019-09-17 10:13:02 +0200

    Changement d'adresse WDG dans documents

--- includes/control/templates/pdf/common/footer.php
+++ includes/control/templates/pdf/common/footer.php
@@ -1,8 +1,8 @@
 <?php // FOOTER ?>
 <page_footer style="width: 100%; margin-top: 20px; text-align: center; font-size: 9pt;">
 	<hr />
-	WE DO GOOD | 8 rue Kervégan - 44000 Nantes | chez Novapuls - 13 Square la Pérouse - 44000 Nantes<br />
-	SAS au capital minimum de 10 000 € - RCS Rennes 797 519 105 - APE 6619B - TVA FR 44 797519105<br />
+	WE DO GOOD | 8 rue Kervégan - 44000 Nantes | Tour Bretagne - Place de Bretagne - 44000 Nantes<br />
+	SAS au capital minimum de 10 000 € - RCS Nantes 797 519 105 - APE 6619B - TVA FR 44 797519105<br />
 	Intermédiaire en Financement Participatif immatriculé à l’ORIAS sous le numéro 17002712<br />
 	Membre de Financement Participatif France, labellisé par FINANCE INNOVATION
 </page_footer>
