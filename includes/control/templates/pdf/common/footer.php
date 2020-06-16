bbf66c0d0d4fe7312b8311599dbb541533e82c7b
emilschn <emilien.schneider@poucr.net>
2020-06-16 16:17:06 +0200

    Mise à jour adresse WE DO GOOD

--- includes/control/templates/pdf/common/footer.php
+++ includes/control/templates/pdf/common/footer.php
@@ -1,7 +1,7 @@
 <?php // FOOTER ?>
 <page_footer style="width: 100%; margin-top: 20px; text-align: center; font-size: 9pt;">
 	<hr>
-	WE DO GOOD | Tour Bretagne - Place de Bretagne - 44000 Nantes<br>
+	WE DO GOOD | 38 rue des Olivettes 44000 Nantes<br>
 	SAS au capital minimum de 10 000 € - RCS Nantes 797 519 105 - APE 6619B - TVA FR 44 797519105<br>
 	Intermédiaire en Financement Participatif immatriculé à l’ORIAS sous le numéro 17002712<br>
 	Membre de Financement Participatif France, labellisé par FINANCE INNOVATION
