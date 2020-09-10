d3069031772e682960d16e4b9fa5fbbcefa11db4
emilschn <emilien.schneider@poucr.net>
2020-09-10 14:08:37 +0200

    Projets : modification requete des projets recherchables

--- includes/data/campaign.php
+++ includes/data/campaign.php
@@ -3786,20 +3786,14 @@ class ATCF_Campaign {
 	
 	public static function list_projects_searchable() {
 		global $wpdb;
-		$results = $wpdb->get_results( "
-			SELECT ID, post_title, post_name FROM ".$wpdb->posts."
-			INNER JOIN ".$wpdb->postmeta." ON ".$wpdb->posts.".ID = ".$wpdb->postmeta.".post_id
-			WHERE ".$wpdb->posts.".post_type = 'download' AND ".$wpdb->posts.".post_status = 'publish' AND ".$wpdb->postmeta.".meta_key = 'campaign_vote' 
-				AND (".$wpdb->postmeta.".meta_value = '".ATCF_Campaign::$campaign_status_vote."' OR ".$wpdb->postmeta.".meta_value = '".ATCF_Campaign::$campaign_status_collecte."' OR ".$wpdb->postmeta.".meta_value = '".ATCF_Campaign::$campaign_status_funded."' OR ".$wpdb->postmeta.".meta_value = '".ATCF_Campaign::$campaign_status_closed."' OR ".$wpdb->postmeta.".meta_value = '".ATCF_Campaign::$campaign_status_archive."')
-			ORDER BY ".$wpdb->posts.".post_date DESC
-		", OBJECT );
+		$results = WDGWPREST_Entity_Project::get_search_list();
 		
 		$buffer = array();
-		foreach ( $results as $project_post ) {
-			$meta_is_hidden = get_post_meta( $project_post->ID, ATCF_Campaign::$key_campaign_is_hidden, TRUE );
-			$meta_project_type = get_post_meta( $project_post->ID, 'campaign_funding_type', TRUE );
+		foreach ( $results as $project_item ) {
+			$meta_is_hidden = get_post_meta( $project_item->wpref, ATCF_Campaign::$key_campaign_is_hidden, TRUE );
+			$meta_project_type = get_post_meta( $project_item->wpref, 'campaign_funding_type', TRUE );
 			if ( empty( $meta_is_hidden ) && $meta_project_type == 'fundingproject' ) {
-				array_push( $buffer, $project_post );
+				array_push( $buffer, $project_item );
 			}
 		}
 		return $buffer;
