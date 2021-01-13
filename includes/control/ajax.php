7a45f393d172b6d8c644135910924927821c7d85
emilschn <emilien.schneider@poucr.net>
2021-01-13 14:38:16 +0100

    Menu : retour Mon compte en traduit

--- includes/control/ajax.php
+++ includes/control/ajax.php
@@ -121,6 +121,7 @@ class WDGAjaxActions {
 			$response[ 'userinfos' ] = array();
 			$response[ 'userinfos' ][ 'userid' ] = $WDGUserCurrent->get_wpref();
 			$response[ 'userinfos' ][ 'username' ] = ( !empty( $firstname_WDGUserCurrent ) ) ? $firstname_WDGUserCurrent : $WDGUserCurrent->get_login();
+			$response[ 'userinfos' ][ 'my_account_txt' ] = __( 'common.MY_ACCOUNT', 'yproject' );
 			$response[ 'userinfos' ][ 'image_dom_element' ] = UIHelpers::get_user_avatar( $WDGUserCurrent->get_wpref(), 'icon' );
 			$response[ 'userinfos' ][ 'logout_url' ] = wp_logout_url(). '&page_id=' .get_the_ID();
 			
