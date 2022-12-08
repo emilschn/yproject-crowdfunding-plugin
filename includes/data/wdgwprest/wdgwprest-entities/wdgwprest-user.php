1c0a7cd6a4aecadb68f062ea42413fe66c4cb516
Hélène Meneuvrier <helene@wedogood.co>
2022-12-08 11:46:03 +0100

    précaution pour éviter une fatal error

--- includes/data/wdgwprest/wdgwprest-entities/wdgwprest-user.php
+++ includes/data/wdgwprest/wdgwprest-entities/wdgwprest-user.php
@@ -122,6 +122,10 @@ class WDGWPREST_Entity_User {
 	 * @return object
 	 */
 	public static function update( WDGUser $user ) {
+		if ( $user->get_wpref() == '' ) {
+			return FALSE;
+		}
+		
 		$parameters = WDGWPREST_Entity_User::set_post_parameters( $user );
 		
 		$result_obj = WDGWPRESTLib::call_post_wdg( 'user/' . $user->get_api_id(), $parameters );
