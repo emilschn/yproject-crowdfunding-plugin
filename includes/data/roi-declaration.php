65b737c282511c636640dc6262beb3b2202f4149
emilschn <emilien.schneider@poucr.net>
2023-03-29 12:32:59 +0200

    Declaration : initialisation : ajustement pour ne pas compter les roi annulés

--- includes/data/roi-declaration.php
+++ includes/data/roi-declaration.php
@@ -1434,7 +1434,7 @@ class WDGROIDeclaration {
 
 		$roi_list = $this->get_rois();
 		foreach ( $roi_list as $roi_item ) {
-			if ( $roi_item->id_investment == $investment_id ) {
+			if ( $roi_item->id_investment == $investment_id && $roi_item->status != WDGROI::$status_canceled ) {
 				$ROI = new WDGROI( $roi_item->id, FALSE, $roi_item );
 				array_push($buffer, $ROI);
 			}
